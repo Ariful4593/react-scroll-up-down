0fb0c2ba240080aaf54b51e542d0f7670925dd1c
Ariful Islam <islam.ariful@sslwireless.com>
2024-01-14 18:20:25 +0600

    .

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: new URL(
-        "./src/index.js",
-        import.meta.url
-      ).pathname,
-      name: "uilibrary",
+      entry: "src/index.js",
+      name: "react-scroll-up-down",
       fileName: "index",
-    },
-    rollupOptions: {
-      external: ["react"],
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
+      formats: ["es"],
     },
   },
-  plugins: [react()],
+  plugins: [react(), libInjectCss()],
 });
