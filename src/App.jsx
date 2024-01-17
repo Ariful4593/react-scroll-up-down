4d1c4cb245903097a8bdd84695134a25123115ae
Ariful Islam <islam.ariful@sslwireless.com>
2024-01-17 16:03:01 +0600

    React and react dom transferred to the dev dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 // import "./App.css";
-// import ScrollUpDown from "./components/ScrollUpDown";
-import ScrollUpDown from "react-top-bottom-scroll";
+import ScrollUpDown from "./components/ScrollUpDown";
+// import ReactAuthSystem from "react-auth-system";
+// import GuestRoute from "./components/AuthenticationSystem/GuestRoute";
+// import Login from "./components/Login/Login";
+// import ProtectedRoute from "./components/AuthenticationSystem/ProtectedRoute";
+// import MasterLayout from "./components/MasterLayout/MasterLayout";
+// import Unauthorized from "./components/AuthenticationSystem/Unauthorized";
+// import NotFound from "./components/AuthenticationSystem/NotFound";
 function App() {
+  // const urls = {
+  //   DASHBOARD: {
+  //     url: "/",
+  //     permission: "web dashboard",
+  //     component: <div>Dashboard</div>,
+  //   },
+  //   UN_AUTHORIZED: {
+  //     url: "/unauthorized",
+  //     permission: null,
+  //     component: <div>Unauthorized</div>,
+  //   },
+  // };
   return (
     <>
       {Array(100)
@@ -9,22 +27,6 @@ function App() {
         .map((_, i) => (
           <div key={i}>{i}</div>
         ))}
-      {/* <ScrollUpDown
-        renderIcon={(scrollDirection) => {
-          return (
-            <i
-              className={
-                scrollDirection === "down"
-                  ? "fas fa-arrow-down"
-                  : "fas fa-arrow-up"
-              }
-            ></i>
-          );
-        }}
-        style={{
-          color: "white",
-        }}
-      /> */}
       {/* <hr /> */}
       <ScrollUpDown
         renderIcon={(scrollDirection) => (
@@ -38,6 +40,34 @@ function App() {
           </div>
         )}
       />
+      {/* <ReactAuthSystem
+        urls={urls}
+        permissionList={[
+          "web dashboard",
+          "ui notification",
+          "organization list",
+        ]}
+        guestRouteComponent={
+          <GuestRoute
+            user={{
+              name: "John Doe",
+              email: "john@gmail.com",
+            }}
+          />
+        }
+        loginComponent={<Login />}
+        protectedRouteComponent={
+          <ProtectedRoute
+            user={{
+              name: "John Doe",
+              email: "john@gmail.com",
+            }}
+          />
+        }
+        masterLayoutComponent={<MasterLayout />}
+        unAuthorizedComponent={<Unauthorized />}
+        notFoundComponent={<NotFound />}
+      /> */}
     </>
   );
 }
